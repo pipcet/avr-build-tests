ca1f9db2b9920115a3d52c7c596b23cfc7a23921
Pip Cet <pipcet@gmail.com>
2020-08-14 10:06:13 +0000

    update for postreload variant

--- test.mk
+++ test.mk
@@ -1,8 +1,10 @@
-MCUS = atmega128 # avr2 avr25 avr3 avr31 avr35 avr5 avr51 avr6 avrxmega3 avrxmega4 avrxmega7 avrtiny 
+EXEC_MCUS = avr31 avr35 avr25 avr3 atmega128
+EXEC_OPTS = 0 1 2 3 s
+MCUS = $(EXEC_MCUS) # avr2 avr25 avr3 avr31 avr35 avr5 avr51 avr6 avrxmega3 avrxmega4 avrxmega7 avrtiny 
 OPTS = 0 1 2 3 s
 COMPARE_OPTS = 2 3 s
 DUMP_RTL = # -fdump-rtl-all -fdump-tree-all
-VARIANTS = ccmode # lra
+VARIANTS = ccmode # postreload lra
 VARIANTS2 = vanilla $(VARIANTS)
 
 $(foreach m,$(MCUS),$(foreach o,$(OPTS),$(eval %.c.{$(m)}.{$(o)}.{ccmode}.s: %.c ; $${CCMODE_GCC} $(DUMP_RTL) -dumpbase $$@ -O$(o) -mmcu=$(m) -S -o $$@ $$< 2> $$@.msg)))
@@ -12,6 +14,14 @@ $(foreach m,$(MCUS),$(foreach o,$(OPTS),$(eval %.c.{$(m)}.{$(o)}.{ccmode-rtl}.s:
 $(foreach m,$(MCUS),$(foreach o,$(OPTS),$(eval %.c.{$(m)}.{$(o)}.{ccmode-tree}.s: %.c ; $${CCMODE_GCC} -fdump-tree-all -dumpbase $$@ -O$(o) -mmcu=$(m) -S -o $$@ $$< 2> $$@.msg)))
 
 $(foreach m,$(MCUS),$(foreach o,$(OPTS),$(eval %.c.{$(m)}.{$(o)}.{ccmode-dP}.s: %.c ; $${CCMODE_GCC} -dP -dumpbase $$@ -O$(o) -mmcu=$(m) -S -o $$@ $$< 2> $$@.msg)))
+
+$(foreach m,$(MCUS),$(foreach o,$(OPTS),$(eval %.c.{$(m)}.{$(o)}.{postreload}.s: %.c ; $${POSTRELOAD_GCC} $(DUMP_RTL) -dumpbase $$@ -O$(o) -mmcu=$(m) -S -o $$@ $$< 2> $$@.msg)))
+
+$(foreach m,$(MCUS),$(foreach o,$(OPTS),$(eval %.c.{$(m)}.{$(o)}.{postreload-rtl}.s: %.c ; $${POSTRELOAD_GCC} -fdump-rtl-all -dumpbase $$@ -O$(o) -mmcu=$(m) -S -o $$@ $$< 2> $$@.msg)))
+
+$(foreach m,$(MCUS),$(foreach o,$(OPTS),$(eval %.c.{$(m)}.{$(o)}.{postreload-tree}.s: %.c ; $${POSTRELOAD_GCC} -fdump-tree-all -dumpbase $$@ -O$(o) -mmcu=$(m) -S -o $$@ $$< 2> $$@.msg)))
+
+$(foreach m,$(MCUS),$(foreach o,$(OPTS),$(eval %.c.{$(m)}.{$(o)}.{postreload-dP}.s: %.c ; $${POSTRELOAD_GCC} -dP -dumpbase $$@ -O$(o) -mmcu=$(m) -S -o $$@ $$< 2> $$@.msg)))
 
 $(foreach m,$(MCUS),$(foreach o,$(OPTS),$(eval %.c.{$(m)}.{$(o)}.{lra}.s: %.c ; $${LRA_GCC} $(DUMP_RTL) -dumpbase $$@ -O$(o) -mmcu=$(m) -S -o $$@ $$< 2> $$@.msg)))
 
@@ -51,14 +61,13 @@ $(foreach v,$(VARIANTS),$(foreach m,$(MCUS),$(foreach o,$(OPTS),$(eval %.c.{$(m)
 .PRECIOUS:
 .SECONDARY:
 
-EXEC_MCUS = atmega128
-EXEC_OPTS = 0 1 2 3 s
-
 $(foreach m,$(EXEC_MCUS),$(foreach o,$(EXEC_OPTS),$(eval %.c.{$(m)}.{$(o)}.{ccmode}.exe: %.c ; $${CCMODE_GCC} $(DUMP_RTL) -dumpbase $$@ -O$(o) -mmcu=$(m) -o $$@ $$< 2> $$@.msg)))
 
 $(foreach m,$(EXEC_MCUS),$(foreach o,$(EXEC_OPTS),$(eval %.c.{$(m)}.{$(o)}.{vanilla}.exe: %.c ; $${VANILLA_GCC} $(DUMP_RTL) -dumpbase $$@ -O$(o) -mmcu=$(m) -o $$@ $$< 2> $$@.msg)))
 
 $(foreach m,$(EXEC_MCUS),$(foreach o,$(EXEC_OPTS),$(eval %.c.{$(m)}.{$(o)}.{lra}.exe: %.c ; $${LRA_GCC} $(DUMP_RTL) -dumpbase $$@ -O$(o) -mmcu=$(m) -o $$@ $$< 2> $$@.msg)))
+
+$(foreach m,$(EXEC_MCUS),$(foreach o,$(EXEC_OPTS),$(eval %.c.{$(m)}.{$(o)}.{postreload}.exe: %.c ; $${POSTRELOAD_GCC} $(DUMP_RTL) -dumpbase $$@ -O$(o) -mmcu=$(m) -o $$@ $$< 2> $$@.msg)))
 
 # %.c.{ccmode}.exe.done: %.c $(foreach m,$(EXEC_MCUS),$(foreach o,$(EXEC_OPTS),%.c.{$(m)}.{$(o)}.{ccmode}.exe))
 #	touch $@
@@ -66,16 +75,19 @@ $(foreach m,$(EXEC_MCUS),$(foreach o,$(EXEC_OPTS),$(eval %.c.{$(m)}.{$(o)}.{lra}
 # %.c.{vanilla}.exe.done: %.c $(foreach m,$(EXEC_MCUS),$(foreach o,$(EXEC_OPTS),%.c.{$(m)}.{$(o)}.{vanilla}.exe))
 #	touch $@
 
-%.exe.status: %.exe
-	(timeout 10 simulavr -B __stop_program -B abort -d atmega128 --file $< -v && echo success || echo aborted) > $@ 2>&1
+$(foreach v,$(VARIANTS2),$(foreach m,$(EXEC_MCUS),$(foreach o,$(EXEC_OPTS),$(eval %.c.{$(m)}.{$(o)}.{$(v)}.exe.status : %.c.{$(m)}.{$(o)}.{$(v)}.exe ; (timeout 10 simulavr -B __stop_program -B abort -d $(m) --file $$< -v && echo success || echo aborted) > $$@ 2>&1))))
 
 %.status.exit: %.status
 	egrep '^MESSAGE Found EP ' $< > $@ || true
 
-%.status.exit.diff: %.{vanilla}.exe.status.exit %.{ccmode}.exe.status.exit
+%.{postreload}.exe.status.exit.diff: %.{vanilla}.exe.status.exit %.{postreload}.exe.status.exit
 	diff -u $^ > $@ || true
 
-%.c.exe.done: $(foreach v,$(VARIANTS),$(foreach m,$(EXEC_MCUS),$(foreach o,$(EXEC_OPTS),%.c.{$(m)}.{$(o)}.{vanilla}.exe.status.exit %.c.{$(m)}.{$(o)}.{$(v)}.exe.status.exit %.c.{$(m)}.{$(o)}.status.exit.diff)))
+%.{ccmode}.exe.status.exit.diff: %.{vanilla}.exe.status.exit %.{ccmode}.exe.status.exit
+	diff -u $^ > $@ || true
+
+#  %.c.{$(m)}.{$(o)}.{postreload}.exe.status.exit.diff
+%.c.exe.done: $(foreach v,$(VARIANTS),$(foreach m,$(EXEC_MCUS),$(foreach o,$(EXEC_OPTS),%.c.{$(m)}.{$(o)}.{vanilla}.exe.status.exit %.c.{$(m)}.{$(o)}.{$(v)}.exe.status.exit %.c.{$(m)}.{$(o)}.{ccmode}.exe.status.exit.diff)))
 	touch $@
 
 %.c.execute: %.c.exe.done
@@ -98,6 +110,24 @@ $(foreach m,$(EXEC_MCUS),$(foreach o,$(EXEC_OPTS),$(eval %.c.{$(m)}.{$(o)}.{ccmo
 	    HASH=`perl ../../diffhash.pl $$D`; \
 	    if ! [ -f ../../classified-diff/*/$$HASH.diff ]; then \
 	        cp $$D ../../diff/$$HASH.diff; \
+	    fi; \
+	done
+
+%.c.{postreload}.diff: $(foreach m,$(MCUS),$(foreach o,$(COMPARE_OPTS),%.c.{$(m)}.{$(o)}.{postreload}.diff))
+	cat $^ > $@
+	for D in $^; do \
+	    HASH=`perl ../../diffhash.pl $$D`; \
+	    if ! [ -f ../../classified-diff/*/$$HASH.diff ]; then \
+	        cp $$D ../../diff-postreload/$$HASH.diff; \
+	    fi; \
+	done
+
+%.c.{postreload}.diff: $(foreach m,$(EXEC_MCUS),$(foreach o,$(EXEC_OPTS),%.c.{$(m)}.{$(o)}.{postreload}.diff))
+	cat $^ > $@
+	for D in $^; do \
+	    HASH=`perl ../../diffhash.pl $$D`; \
+	    if ! [ -f ../../classified-diff/*/$$HASH.diff ]; then \
+	        cp $$D ../../diff-postreload/$$HASH.diff; \
 	    fi; \
 	done
 
